b6bc2544785d048952aeb484f38133c975e8c4cc
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-05-01 13:56:20 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  let radius = parseInt(document.getElementById('radius').value);
+
+
+  // process
+  let volume = radius ** 3 * Math.PI * (4/3);
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' cm3'
+
+}
